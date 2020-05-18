4e9e97db5a5b8d6b41285cad91918eee9b09c1df
nslyc <273217471@qq.com>
2020-05-18 17:58:31 +0800

    daily code

---
+++ easy/303.区域和检索-数组不可变.js
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=javascript
+ *
+ * [303] 区域和检索 - 数组不可变
+ *
+ * https://leetcode-cn.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (60.84%)
+ * Likes:    157
+ * Dislikes: 0
+ * Total Accepted:    38.4K
+ * Total Submissions: 62.4K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ * 
+ * 示例：
+ * 
+ * 给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+ * 
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 
+ * 说明:
+ * 
+ * 
+ * 你可以假设数组不可变。
+ * 会多次调用 sumRange 方法。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+    this.nums = Array.from(nums);
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (i, j) {
+    let sum = 0;
+    while (i <= j) {
+        sum += this.nums[i];
+        i++;
+    }
+    return sum;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
+// @lc code=end
---
+++ easy/303.区域和检索-数组不可变_缓存.js
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=javascript
+ *
+ * [303] 区域和检索 - 数组不可变
+ *
+ * https://leetcode-cn.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (60.84%)
+ * Likes:    157
+ * Dislikes: 0
+ * Total Accepted:    38.4K
+ * Total Submissions: 62.4K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ * 
+ * 示例：
+ * 
+ * 给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+ * 
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 
+ * 说明:
+ * 
+ * 
+ * 你可以假设数组不可变。
+ * 会多次调用 sumRange 方法。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+    this.nums = Array.from(nums);
+    this.sumCache = new Array(nums.length).fill(0);
+    for (let i = 0; i < nums.length; i++) {
+        this.sumCache[i + 1] = this.sumCache[i] + nums[i];
+    }
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (i, j) {
+    return this.sumCache[j + 1] - this.sumCache[i];
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
+// @lc code=end
