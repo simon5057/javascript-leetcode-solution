930929730f79790fbf0585fc275e60d38a5455aa
nslyc <273217471@qq.com>
2020-06-28 18:10:50 +0800

    daily code

---
+++ easy/538.把二叉搜索树转换为累加树.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=538 lang=javascript
+ *
+ * [538] 把二叉搜索树转换为累加树
+ *
+ * https://leetcode-cn.com/problems/convert-bst-to-greater-tree/description/
+ *
+ * algorithms
+ * Easy (61.43%)
+ * Likes:    271
+ * Dislikes: 0
+ * Total Accepted:    29.2K
+ * Total Submissions: 47.1K
+ * Testcase Example:  '[5,2,13]'
+ *
+ * 给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater
+ * Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+ * 
+ * 
+ * 
+ * 例如：
+ * 
+ * 输入: 原始二叉搜索树:
+ * ⁠             5
+ * ⁠           /   \
+ * ⁠          2     13
+ * 
+ * 输出: 转换为累加树:
+ * ⁠            18
+ * ⁠           /   \
+ * ⁠         20     13
+ * 
+ * 
+ * 
+ * 
+ * 注意：本题和 1038:
+ * https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/ 相同
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBST = function (root) {
+    let cur = 0;
+    traverse(root, function (node) {
+        let temp = node.val;
+        node.val += cur;
+        cur += temp;
+    });
+    return root;
+};
+
+function traverse(root, fn) {
+    if (root) {
+        traverse(root.right, fn);
+        fn(root);
+        traverse(root.left, fn);
+    }
+}
+// @lc code=end
