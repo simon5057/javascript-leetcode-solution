17fec5e520a97bba4651131b7ac71cb826665d2f
lyc <273217471@qq.com>
2020-07-04 17:56:52 +0800

    easy

---
+++ easy/tree/653.两数之和-iv-输入-bst_一次遍历.js
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=653 lang=javascript
+ *
+ * [653] 两数之和 IV - 输入 BST
+ *
+ * https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/description/
+ *
+ * algorithms
+ * Easy (55.14%)
+ * Likes:    151
+ * Dislikes: 0
+ * Total Accepted:    17.4K
+ * Total Submissions: 31.4K
+ * Testcase Example:  '[5,3,6,2,4,null,7]\n9'
+ *
+ * 给定一个二叉搜索树和一个目标结果，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+ * 
+ * 案例 1:
+ * 
+ * 
+ * 输入: 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 3   6
+ * ⁠/ \   \
+ * 2   4   7
+ * 
+ * Target = 9
+ * 
+ * 输出: True
+ * 
+ * 
+ * 
+ * 
+ * 案例 2:
+ * 
+ * 
+ * 输入: 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 3   6
+ * ⁠/ \   \
+ * 2   4   7
+ * 
+ * Target = 28
+ * 
+ * 输出: False
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function (root, k) {
+    const arr = [];
+    const stack = [root];
+    while (stack.length) {
+        let cur = stack.pop();
+        arr.push(cur.val);
+        if (cur.right) stack.push(cur.right);
+        if (cur.left) stack.push(cur.left);
+    }
+    let map = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (map[k - arr[i]] != null) return true;
+        map[arr[i]] = arr[i];
+    }
+    return false;
+};
+// @lc code=end
