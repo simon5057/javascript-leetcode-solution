76bb4659083e404a59f63ed2ea4a00496095658f
nslyc <273217471@qq.com>
2020-07-21 10:02:44 +0800

    sort

---
+++ ts-easy/string/917.仅仅反转字母_栈.ts
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=917 lang=typescript
+ *
+ * [917] 仅仅反转字母
+ *
+ * https://leetcode-cn.com/problems/reverse-only-letters/description/
+ *
+ * algorithms
+ * Easy (55.61%)
+ * Likes:    52
+ * Dislikes: 0
+ * Total Accepted:    14.6K
+ * Total Submissions: 26.3K
+ * Testcase Example:  '"ab-cd"'
+ *
+ * 给定一个字符串 S，返回 “反转后的” 字符串，其中不是字母的字符都保留在原地，而所有字母的位置发生反转。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入："ab-cd"
+ * 输出："dc-ba"
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入："a-bC-dEf-ghIj"
+ * 输出："j-Ih-gfE-dCba"
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入："Test1ng-Leet=code-Q!"
+ * 输出："Qedo1ct-eeLg=ntse-T!"
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * S.length <= 100
+ * 33 <= S[i].ASCIIcode <= 122 
+ * S 中不包含 \ or "
+ * 
+ * 
+ */
+
+// @lc code=start
+function reverseOnlyLetters(S: string): string {
+    const stack: string[] = [];
+    for (let i = 0; i < S.length; i++) {
+        if (/[a-zA-Z]/.test(S[i])) {
+            stack.push(S[i]);
+        }
+    }
+
+    let res: string = '';
+    for (let i = 0; i < S.length; i++) {
+        if (/[a-zA-Z]/.test(S[i])) {
+            res += stack.pop();
+        } else {
+            res += S[i];
+        }
+    }
+    return res;
+};
+// @lc code=end
+
