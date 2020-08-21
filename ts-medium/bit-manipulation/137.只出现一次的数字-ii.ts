0a919f3d206b73394e8ad3a21934e406f05abc15
nslyc <273217471@qq.com>
2020-08-21 18:00:40 +0800

    daily

---
+++ ts-medium/bit-manipulation/137.只出现一次的数字-ii.ts
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=137 lang=typescript
+ *
+ * [137] 只出现一次的数字 II
+ *
+ * https://leetcode-cn.com/problems/single-number-ii/description/
+ *
+ * algorithms
+ * Medium (67.78%)
+ * Likes:    402
+ * Dislikes: 0
+ * Total Accepted:    38.8K
+ * Total Submissions: 57.3K
+ * Testcase Example:  '[2,2,3,2]'
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现了三次。找出那个只出现了一次的元素。
+ * 
+ * 说明：
+ * 
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * 
+ * 示例 1:
+ * 
+ * 输入: [2,2,3,2]
+ * 输出: 3
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [0,1,0,1,0,1,99]
+ * 输出: 99
+ * 
+ */
+
+// @lc code=start
+function singleNumber(nums: number[]): number {
+    const map: { [key: number]: number } = Object.create(null);
+    for (const n of nums) {
+        if (!map[n]) {
+            map[n] = 1;
+        } else {
+            map[n]++;
+        }
+    }
+    for (const k in map) {
+        if (map[k] === 1) return Number(k);
+    }
+    return -1;
+};
+// @lc code=end
+
