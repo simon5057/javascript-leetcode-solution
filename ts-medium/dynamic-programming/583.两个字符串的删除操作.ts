3f6f3c74851d8e21c07d03d2d9d7b6ccb9738e8f
lyc <273217471@qq.com>
2020-11-17 22:37:34 +0800

    daily

---
+++ ts-medium/dynamic-programming/583.两个字符串的删除操作.ts
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=583 lang=typescript
+ *
+ * [583] 两个字符串的删除操作
+ *
+ * https://leetcode-cn.com/problems/delete-operation-for-two-strings/description/
+ *
+ * algorithms
+ * Medium (50.67%)
+ * Likes:    158
+ * Dislikes: 0
+ * Total Accepted:    11.5K
+ * Total Submissions: 22.6K
+ * Testcase Example:  '"sea"\n"eat"'
+ *
+ * 给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+ * 
+ * 
+ * 
+ * 示例：
+ * 
+ * 输入: "sea", "eat"
+ * 输出: 2
+ * 解释: 第一步将"sea"变为"ea"，第二步将"eat"变为"ea"
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 给定单词的长度不超过500。
+ * 给定单词中的字符只含有小写字母。
+ * 
+ * 
+ */
+
+// @lc code=start
+function minDistance(word1: string, word2: string): number {
+    let m = word1.length;
+    let n = word2.length;
+    let dp: number[][] = Array.from({ length: m + 1 }, row => Array.from({ length: n + 1 }, () => 0));
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i === 0 || j === 0) continue;
+            if (word1[i - 1] === word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    return m + n - (2 * dp[m][n]);
+};
+// @lc code=end
+
